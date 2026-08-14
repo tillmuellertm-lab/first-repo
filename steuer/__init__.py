a2a30bd11a6e3448496df481a5d837d801c4403c
tillmuellertm-lab <tillmuellertm@gmail.com>
2026-08-14 18:01:19 +0200

    Create __init__.py

---
+++ steuer/__init__.py
@@ -0,0 +1,9 @@
+"""Steuer-Assistent: Aufbereitung eingescannter Unterlagen fuer die deutsche
+Einkommensteuererklaerung als Zuarbeit fuer den Steuerberater.
+
+Das Werkzeug ersetzt keine Steuerberatung. Alle Einordnungen, Betraege und
+Hinweise sind Vorschlaege und muessen fachlich geprueft werden.
+"""
+
+__version__ = "0.1.0"
+__all__ = ["__version__"]
